7367d2c3db96581aeeb0524bbc22f0277c4187a6
hysperion <bestmark77@gmail.com>
2023-03-23 20:24:53 +0900

    picture over rlcertification

--- lib/screens/home/rlcertificationview.dart
+++ lib/screens/home/rlcertificationview.dart
@@ -39,51 +39,61 @@ class _RLCertificationViewState extends State<RLCertificationView> {
           ),
         ),
       ),
-      body: Center(
-        child: Container(
-          width: 342.w,
-          height: 161.h,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: const <BoxShadow> [
-              BoxShadow(
-                color: Color(0xffD3D3D3),
-                blurRadius: 10.0,
-                offset: Offset(0.0, 5),
-              ),
-            ],
-            color: const Color(0xffFFFFFF),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: 342.w,
+            height: 200.h,
+            margin: EdgeInsets.only(left: 24.w, right: 24.w, top: 24.h, bottom: 24.h),
+            child: Image.asset(
+              "assets/images/example.png",
+              fit: BoxFit.fill,
+            ),
           ),
-          child: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(top: 35.h),
-                child: const Text(
-                  "Arrived at the Red light section",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w700,
-                    color: Color(0xff000000),
+          Container(
+            width: 342.w,
+            height: 161.h,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: const <BoxShadow> [
+                BoxShadow(
+                  color: Color(0xffD3D3D3),
+                  blurRadius: 10.0,
+                  offset: Offset(0.0, 5),
+                ),
+              ],
+              color: const Color(0xffFFFFFF),
+            ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 35.h),
+                  child: const Text(
+                    "Arrived at the Red light section",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xff000000),
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 7.h),
-                child: const Text(
-                  "Please pick up the trash nearby",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w300,
-                    color: Color(0xff000000),
+                Container(
+                  margin: EdgeInsets.only(top: 7.h),
+                  child: const Text(
+                    "Please pick up the trash nearby",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                      color: Color(0xff000000),
+                    ),
                   ),
                 ),
-              ),
-              InkWell(
-                child: Container(
-                  margin: EdgeInsets.only(top: 29.h, left: 12.w, right: 12.w),
-                  height: 48.h,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
+                InkWell(
+                  child: Container(
+                    margin: EdgeInsets.only(top: 29.h, left: 12.w, right: 12.w),
+                    height: 48.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
                     color: const Color(0xff5DC86C),
                   ),
                   child: const Center(
@@ -93,15 +103,16 @@ class _RLCertificationViewState extends State<RLCertificationView> {
                         fontSize: 18,
                         fontWeight: FontWeight.w700,
                         color: Color(0xffFFFFFF),
+                        ),
                       ),
                     ),
                   ),
+                  onTap: () {},
                 ),
-                onTap: () {},
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
